79791cc975141c59e45ad0efa5a5a009988d8c6b
Donny <EDL1155@gmail.com>
2016-09-24 08:36:50 -0700

    pseudo code added

---
+++ pseudo_code.rb
@@ -0,0 +1,69 @@
+#a method that returns the sum of two integers
+
+Ask the user for the first number
+  
+Ask the user for the second number
+  
+Add the two numbers together
+  
+Return the value
+  
+END  
+  
+  
+  
+START
+  
+GET the first number
+
+SET the first number
+  
+GET the second number
+  
+SET the second number
+
+SET the result of the two numbers added together
+  
+PRINT the result
+  
+
+#a method that takes an array of strings, and returns a string that is all those strings concatenated together.
+  
+given an array of strings
+  
+take each item in the array and concatenate them together
+
+return the result
+  
+  
+START  
+  
+SET an array
+  
+ITERATE through the whole given array
+  
+PUT a string that concatenates all the items of the array
+  
+END
+  
+  
+# a method that takes an array of integers, and returns a new array with every #other element.
+  
+1. pass an array with integers
+  
+2. select every other integer from the given array
+  
+3. create a new array with the integers from step 2.
+    
+  
+START
+  
+SET an array of integers
+  
+ITERATE through the array selecting every other integer
+  
+SET a new array from the integers collected from the iteration above.
+    
+END  
+  
+  
